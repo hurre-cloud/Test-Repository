d46e2b6c1dd0339d5c4d757467c5539a91292663
Jully Jain <juli.mits@gmail.com>
2021-10-10 07:22:19 +0100

    Primary constructor code

---
+++ src/Excercises/AuxiliaryConstructorForCaseClass.scala
@@ -0,0 +1,18 @@
+package Excercises
+
+import Excercises.CaseDemo.Person
+
+import scala.io.Codec.fallbackSystemCodec.name
+
+class AuxiliaryConstructorForCaseClass {
+
+
+}
+
+case class Person(var name: String, var age: Int)
+
+
+
+
+
+
