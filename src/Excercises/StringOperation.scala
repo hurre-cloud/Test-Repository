b4a566af0876a2ed505ac1fccf39614242d2f5ff
Jully Jain <juli.mits@gmail.com>
2021-10-24 05:17:02 +0100

    Primary constructor code

--- src/Excercises/StringOperation.scala
+++ src/Excercises/StringOperation.scala
@@ -5,6 +5,24 @@ object StringOperation extends App {
   val s = "hello"
 
   s.foreach(println)
+  println(s.filter(x => x != 'l'))
 
+  println(s.drop(3).take(1).capitalize)
+
+  println(s.compareTo("hel"))
+
+  //for loop with yeild
+  val result = for {
+    c <- s
+    if c != 'h'
+  } yield c.toUpper
+  println(result)
+
+  val result1 = s.filter(_ != 'h').map(_.toUpper)
+  println(result1)
+
+  val func = (c: Char) => c.toUpper
+  //println(func)
+  println(s.map(func))
 
 }
